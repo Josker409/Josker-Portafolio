8981a8dc1fce6335161b84e5936963ba19f7b986
Josker Guzman <joskercoderhouse@gmail.com>
2024-04-21 22:53:52 -0300

    Dando forma a la web

---
+++ main.js
@@ -0,0 +1,10 @@
+// alert ("Bienvenido, escribe tu nombre");
+
+// const nombre = prompt("Ingresa tu nombre por favor");
+
+
+// if ((nombre == "Josker") || (nombre == "Maria")) {
+//     alert ("Hola, bienvenido/a " + nombre);
+// } else {
+//     alert("Hola, binevenido/a");
+// }
